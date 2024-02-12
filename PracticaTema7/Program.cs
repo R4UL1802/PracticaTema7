bb43c0cfb8a2b31cd341e50cc5764e5c4d63309f
W7 Java <W7 Java@W7JAVA-PC>
2024-02-12 10:34:09 +0100

    Agregar archivos de proyecto.

---
+++ PracticaTema7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PracticaTema7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Hola
+        }
+    }
+}
