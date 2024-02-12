e59fd4e62772224b723172db6801ab2aea847f6e
R4UL1802 <estudioconedmodo@gmail.com>
2024-02-12 10:48:20 +0100

    HOla

--- PracticaTema7/Program.cs
+++ PracticaTema7/Program.cs
@@ -7,7 +7,7 @@ namespace PracticaTema7
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //Hola
+            //Hola tu mamama
         }
     }
 }
