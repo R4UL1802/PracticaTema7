cacfec93b6731ad57edf7cf97637f851009590ef
vinchi12 <vinchibetix@gmail.com>
2024-02-13 10:38:08 +0100

    cambios6

---
+++ PracticaTema7/DatosAminoacidos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticaTema7
+{
+    public enum TipoAminoacido
+    { Valina, Leucina, Treonina, Lisina, Triptofano, Histidina,
+        Fenilalanina, Isoleucina, Arginina, Metionina, Alanina, Prolina, Glicina, Serina,
+        Cisteina, Asparagina, Glutamina, Tirosina, AcidoAspartico, Acidoglutamico, STOP }
+
+
+    class DatosAminoacidos
+    {
+        public static Dictionary<TipoAminoacido, string> nombresaminoacidos = new Dictionary<TipoAminoacido, string>()
+        { 
+            //En esta primera pon los esenciales
+        {TipoAminoacido.Valina, "Valina" },
+        {TipoAminoacido.Leucina, "Leu"},
+        {TipoAminoacido.Treonina, "Treonina" },
+        {TipoAminoacido.Lisina, "Lisina" },
+        {TipoAminoacido.Triptofano, "Triptofano" },
+            {TipoAminoacido.Histidina, "Histidina" },
+            {TipoAminoacido.Fenilalanina, "Fenilalanina" },
+            {TipoAminoacido.Isoleucina, "Isoleucina" },
+            {TipoAminoacido.Arginina, "Arginina" },
+            //En esta segunda pon no esenciales
+            {TipoAminoacido.Alanina, "Alanina" },
+
+
+        }
+        public static Dictionary<TipoAminoacido, string> nombrecorto = new Dictionary<TipoAminoacido, string>()
+        {   //vas poniendo las abreviaturas 
+            { TipoAminoacido.Valina, "VAL"}, 
+
+
+} 
+        public static Dictionary<TipoAminoacido, >
+        }
+}
--- PracticaTema7/Program.cs
+++ PracticaTema7/Program.cs
@@ -4,7 +4,6 @@ namespace PracticaTema7
 {
     class Program
     {
-        public enum Aminoacidos {zapato }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
