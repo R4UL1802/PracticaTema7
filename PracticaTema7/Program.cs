b4f5c80e7b490ab9c5414c1e5dfd4c02db5ecc5a
vinchi12 <vinchibetix@gmail.com>
2024-02-13 08:43:54 +0100

    cambio2

--- PracticaTema7/Program.cs
+++ PracticaTema7/Program.cs
@@ -4,10 +4,11 @@ namespace PracticaTema7
 {
     class Program
     {
+        public enum Aminoacidos { }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //Hola
+            
         }
     }
 }
